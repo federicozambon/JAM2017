210404f5d7b4605809a31b227965e30ed5d49e37
dbga_29 <dbga_29@dbga.local>
2017-02-07 17:05:14 +0100

    no message

---
+++ Assets/Scripts/BackGroundPicker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackGroundPicker : MonoBehaviour
+{
+    public string backGroundID;
+    SpriteRenderer sr;
+
+	void Awake ()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        switch (backGroundID)
+        {
+            case "guardalinee":
+                sr.sprite = Resources.Load<Sprite> ("Scena1");
+                break;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/UiController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UiController : MonoBehaviour
+{
+    public Slider timeSlider;
+    float timer = 0f;
+    float timeToAnswer = 3f;
+
+	IEnumerator PlayerChoice()
+    {
+        while (timer <= timeToAnswer)
+        {
+            timer += Time.deltaTime;
+            timeSlider.value -= timer / timeToAnswer;
+            yield return null;
+        }
+        
+	}
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(PlayerChoice());
+        }
+	}
+}
