2405f0c3292c7df35647f66253a173778e3d085b
dbga_30 <dbga_30@dbga.local>
2017-02-08 12:55:43 +0100

    Implementate Scene menu,crediti,howto, quit.

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void StartMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Credits()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void QuitMenu()
+    {
+        Application.Quit();
+    }
+}
